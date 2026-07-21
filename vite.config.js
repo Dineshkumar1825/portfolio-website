f7dd191b6dec7dc5e0c9fc0cd396980de570c270
Dineshkumar <dineshkumar18251@gmail.com>
2026-07-21 20:14:10 +0530

    Added node modules in git ignore

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/dinesh-portfolio/',
-  plugins: [react()],
-  // server: {
-  //   port: 3000,
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:5000',
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // },
+  base: '/portfolio-website/',
+  plugins: [react()]
 });
